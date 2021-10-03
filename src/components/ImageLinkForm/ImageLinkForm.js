32a6653599ec9429805c904f5564d83dd3f68279
William Hamilton <draxy80@gmail.com>
2021-10-04 05:45:07 +1000

    Missed one component for zindex

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-import './ImageLinkForm.css';
-
+import "./ImageLinkForm.css";
 
 const ImageLinkForm = () => {
   return (
     <div>
-      <p className="f3 center zin">
-        {"Detect faces in your pictures. Give it a try!"}
-      </p>
+      <div className="center">
+        <p className="zin white f3 center">
+          {"Detect faces in your pictures. Give it a try!"}
+        </p>
+      </div>
       <div className="center">
         <div className="center form pa4 br3 shadow-5 carbon zin">
           <input className="f4 pa2 w-70 center zin" type="text" />
