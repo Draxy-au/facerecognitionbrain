c315c8b77b6cf87fad6fd623ca5d53dcdadd94f1
William Hamilton <draxy80@gmail.com>
2021-10-04 05:32:48 +1000

    missed one element with zindex

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -6,7 +6,7 @@ import './ImageLinkForm.css';
 const ImageLinkForm = () => {
   return (
     <div>
-      <p className="f3 center">
+      <p className="f3 center zin">
         {"Detect faces in your pictures. Give it a try!"}
       </p>
       <div className="center">
