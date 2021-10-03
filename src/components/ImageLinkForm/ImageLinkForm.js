6812195459d44dcb4e2ae92a2562b7f816bdf630
William Hamilton <draxy80@gmail.com>
2021-10-04 03:22:20 +1000

    Updated text

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,7 +7,7 @@ const ImageLinkForm = () => {
   return (
     <div>
       <p className="f3 center">
-        {"This Magic Brain will detect faces in your pictures. Give it a try!"}
+        {"Detect faces in your pictures. Give it a try!"}
       </p>
       <div className="center">
         <div className="center form pa4 br3 shadow-5 carbon">
